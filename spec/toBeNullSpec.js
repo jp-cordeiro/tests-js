dc4d9cd0a726e2deeb5a92f778777ec5eddf040c
jp-cordeiro <jpc.araujo@hotmail.com>
2019-06-15 23:26:13 -0300

    toBeNull

---
+++ spec/toBeNullSpec.js
@@ -0,0 +1,10 @@
+/**
+ * Compara se um objeto é nulo, ou seja, se ele é null
+ */
+describe('toBeNull', function () {
+    it('should test toBeNull', function () {
+        let nullVar = null
+
+        expect(nullVar).toBeNull()
+    })
+})
